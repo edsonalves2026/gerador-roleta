89ce9e7dd2a53cda80fc83a0db887766beefe26b
edsonalves2026 <edson-evaristo@hotmail.com>
2026-09-06 14:15:03 -0300

    Update roleta2.app.py

--- roleta2.app.py
+++ roleta2.app.py
@@ -975,8 +975,8 @@ if qtd >= 20:
     </div>
     """
 
-    st.markdown(html_racetrack, unsafe_html=True)
-    st.caption("🌡️ Distribuição Racetrack | As cores esquentam dinamicamente conforme a frequência das últimas 200 rodadas.")
+st.markdown(html_racetrack, unsafe_html=True)
+st.caption("🌡️ Distribuição Racetrack | As cores esquentam dinamicamente conforme a frequência das últimas 200 rodadas.")
 else:
     st.info(f"Dados insuficientes para mapa de calor no Racetrack ({qtd}/20)")
 
